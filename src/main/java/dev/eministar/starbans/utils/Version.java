9fed18fed5ae46d8b6426b2fbe520649d5f989d4
Nael-Emin Ben Oun <benounnaelemin@gmail.com>
2026-03-10 19:05:24 +0100

    Bump version to 1.1.0 in pom.xml and Version.java

--- src/main/java/dev/eministar/starbans/utils/Version.java
+++ src/main/java/dev/eministar/starbans/utils/Version.java
@@ -4,7 +4,7 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public final class Version {
 
-    private static String version = "1.0.0";
+    private static String version = "1.1.0";
 
     private Version() {
     }
